9bcfa91a96c4cf5320bec380e933a4143b4fae70
Dylan Jaide White <dylan_j_white@yahoo.co.uk>
2025-12-16 22:27:45 +0000

    Fix name regex
    Previously assumed all column 2 values were names in the format <FIRST INITIAL> <SURNAME>, eg. J SMITH. However, in some cases multiple first initials or full first names are used. Others do not have names assigned, and have the value "Vacancy" (case-sensitive).

--- gwr_links_to_calendar/utils.py
+++ gwr_links_to_calendar/utils.py
@@ -2,7 +2,7 @@ TIME_REGEX = r"\d{1,2}:\d{2}"
 DURATION_REGEX = r"\d{1,2}.\d{2}"
 DATE_REGEX = r"\d*/\d*/\d*"
 
-NAME_REGEX = r"[A-Z] [A-Z]+"
+NAME_REGEX = r"([A-Z]+ [A-Z]+|Vacancy)"
 ROW_NUMBER_REGEX = r"[1-9]\d*"
 WEEKLY_HOURS_REGEX = r"\d* hrs \d* mins"
 SHIFT_DETAILS_REGEXES = {
